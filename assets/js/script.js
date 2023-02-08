196533e97659ef128e0896010b1763dc3e67b303
Cobbina Emmanuel <kobbyj099@gmail.com>
2023-02-08 14:48:24 +0000

    Top Downloaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,26 @@ $(".owl-carousel").owlCarousel({
   autoplaytimeout: 100,
   items: 3,
   nav: true,
-  loop: true,
+    loop: true,
+    responsive: {
+        0: {
+            items: 1,
+            dots:true
+        },
+        485: {
+            items: 1,
+            dots:true
+        },
+        728: {
+            items: 2,
+            dots:true
+        }
+  }
 });
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach(item => {
+    item.addEventListener('click', function () {
+        item.getElementsByClassName.color = 'deeppink';
+        
+    })
+})
