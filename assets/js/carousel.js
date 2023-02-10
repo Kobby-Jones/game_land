ca846f29e192c617e9c624021a9b11cfe12a33f8
Cobbina Emmanuel <kobbyj099@gmail.com>
2023-02-10 03:42:09 +0000

    Modified

---
+++ assets/js/carousel.js
@@ -0,0 +1,53 @@
+$(".carousel1").owlCarousel({
+  autoplay: true,
+  autoplayhoverpause: true,
+  autoplaytimeout: 100,
+  items: 3,
+  nav: true,
+    loop: true,
+    responsive: {
+        0: {
+            items: 1,
+            dots: true,
+            nav: true
+        },
+        485: {
+            items: 1,
+            dots: true,
+            nav: true
+        },
+        728: {
+            items: 2,
+            dots: true,
+            nav: true,
+        }
+  }
+});
+$(".carousel2").owlCarousel({
+  autoplay: true,
+  autoplayhoverpause: true,
+  autoplaytimeout: 100,
+  items: 3,
+  nav: true,
+  loop: true,
+  responsive: {
+    0: {
+      items: 1,
+      dots: true,
+      nav: true,
+    },
+    485: {
+      items: 1,
+      dots: true,
+      nav: true,
+    },
+    728: {
+      items: 3,
+      dots: true,
+      nav: true,
+    },
+  },
+});
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,8 @@
-$(".carousel1").owlCarousel({
-  autoplay: true,
-  autoplayhoverpause: true,
-  autoplaytimeout: 100,
-  items: 3,
-  nav: true,
-    loop: true,
-    responsive: {
-        0: {
-            items: 1,
-            dots: true,
-            nav: true
-        },
-        485: {
-            items: 1,
-            dots: true,
-            nav: true
-        },
-        728: {
-            items: 2,
-            dots: true,
-            nav: true,
-        }
-  }
-});
-$(".carousel2").owlCarousel({
-  autoplay: true,
-  autoplayhoverpause: true,
-  autoplaytimeout: 100,
-  items: 3,
-  nav: true,
-  loop: true,
-  responsive: {
-    0: {
-      items: 1,
-      dots: true,
-      nav: true,
-    },
-    485: {
-      items: 1,
-      dots: true,
-      nav: true,
-    },
-    728: {
-      items: 3,
-      dots: true,
-      nav: true,
-    },
-  },
-});
-const navLink = document.querySelectorAll(".nav-link");
-navLink.forEach(item => {
-    item.addEventListener('click', function () {
-        item.getElementsByClassName.color = 'deeppink';
-        
-    })
+const search = document.querySelector(".search-btn");
+search.addEventListener('click', function () {
+    alert("Hello")
 })
-
-
-const mostPopularStreamRow = document.querySelector(
-  ".most-popular-stream-row"
-);
-
-
+const browse = document.getElementById('browse-now');
+browse.addEventListener('click', function () {
+    window.location.href = "/browse.html"
+})
