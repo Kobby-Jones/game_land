01843ec3a4fae201d5646ae75da318baf5afb07d
Cobbina Emmanuel <kobbyj099@gmail.com>
2023-02-20 15:35:02 +0000

    Added More games to the library

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,27 +14,136 @@ const libraryDate1 = document.getElementById("library-date-1");
 const libraryDate2 = document.getElementById("library-date-2");
 const libraryDate3 = document.getElementById("library-date-3");
 const viewYourLibraryButton = document.querySelector(".view-library-btn");
+const discoverPopularBtn = document.querySelector(".discover-btn");
+const browseNowBtn = document.getElementById("browse-now");
+const downloadBtn1 = document.querySelector(".download1");
+const downloadBtn2 = document.querySelector(".download2");
+const downloadBtn3 = document.querySelector(".download3");
 
-let gameLibrary1 = games[Math.floor(Math.random() * games.length)]
-let gameLibrary2 = games[Math.floor(Math.random() * games.length)]
-let gameLibrary3 = games[Math.floor(Math.random() * games.length)]
+const libraryContainer = document.querySelector('.library-container');
 
-libraryImage1.src = gameLibrary1.thumbnail;
-libraryTitle1.innerText = gameLibrary1.title;
-libraryDeveloper1.innerText = gameLibrary1.developer;
-libraryDate1.innerText = gameLibrary1.release_date;
-libraryImage2.src = gameLibrary2.thumbnail;
-libraryTitle2.innerText = gameLibrary2.title;
-libraryDeveloper2.innerText = gameLibrary2.developer;
-libraryDate2.innerText = gameLibrary2.release_date;
-libraryImage3.src = gameLibrary3.thumbnail;
-libraryTitle3.innerText = gameLibrary3.title;
-libraryDeveloper3.innerText = gameLibrary3.developer;
-libraryDate3.innerText = gameLibrary3.release_date;
+// Crete the library game function
 
-viewYourLibraryButton.addEventListener('click', function () {
+function libraryGames(game) {
+  // Crete the main row
+  let library_Row = document.createElement("div");
+  library_Row.classList.add("row");
+  // Create the image column
+  let image_Column = document.createElement("div")
+  image_Column.classList.add("col-lg-2");
+  // Create the card for the image
+  let image_Card = document.createElement("div");
+  image_Card.classList.add("card", "bg-dark");
+  let library_Image = document.createElement("img");
+  library_Image.classList.add("card-img", "rounded-4");
+  library_Image.alt = "";
+  library_Image.src = game.thumbnail;
+  image_Card.appendChild(library_Image);
+  image_Column.appendChild(image_Card);
+  library_Row.appendChild(image_Column);
+
+  // Create a column for the title and developer
+  let title_Developer_Column = document.createElement("div");
+  title_Developer_Column.classList.add("col-lg-2");
+  let library_Title_div = document.createElement("div");
+  library_Title_div.classList.add("title", "text-light", "mt-5", "text-center");
+  library_Title_div.innerText = game.title;
+  let library_Developer_div = document.createElement("div");
+  library_Developer_div.classList.add("text-secondary", "text-center");
+  library_Developer_div.innerText = game.developer;
+  // Add the title and developer to the column and add the column to the row
+  title_Developer_Column.appendChild(library_Title_div);
+  title_Developer_Column.appendChild(library_Developer_div);
+  library_Row.appendChild(title_Developer_Column);
+// Create column for the date added
+  let date_Added_Column = document.createElement("div");
+  date_Added_Column.classList.add("col-lg-2");
+  let date_Added = document.createElement("div");
+  date_Added.classList.add("date-added", "text-light", "mt-5", "text-center");
+  date_Added.innerText = "Released Date";
+  let released_Date = document.createElement("div");
+  released_Date.classList.add("release-date", "text-secondary", "text-center");
+  released_Date.innerText = game.release_date;
+  // Add date to column and add column to row
+  date_Added_Column.appendChild(date_Added);
+  date_Added_Column.appendChild(released_Date);
+  library_Row.appendChild(date_Added_Column);
+
+//  Create Platform column
+   let platform_Column = document.createElement("div");
+   platform_Column.classList.add("col-lg-2");
+   let platform = document.createElement("div");
+   platform.classList.add("date-added", "text-light", "mt-5", "text-center");
+   platform.innerText = "Platform";
+   let platform_Name = document.createElement("div");
+   platform_Name.classList.add("release-date", "text-secondary", "text-center");
+   platform_Name.innerText = game.platform;
+   // Add platform to column and add column to row
+   platform_Column.appendChild(platform);
+   platform_Column.appendChild(platform_Name);
+  library_Row.appendChild(platform_Column);
+  
+// Create the Genre Column
+   let genre_Column = document.createElement("div");
+   genre_Column.classList.add("col-lg-2");
+   let genre = document.createElement("div");
+   genre.classList.add("date-added", "text-light", "mt-5", "text-center");
+   genre.innerText = "Genre";
+   let genre_Name = document.createElement("div");
+   genre_Name.classList.add("release-date", "text-secondary", "text-center");
+   genre_Name.innerText = game.genre;
+   // Add genre to column and add column to row
+   genre_Column.appendChild(genre);
+   genre_Column.appendChild(genre_Name);
+   library_Row.appendChild(genre_Column);
+
+  // Create download column
+  let download_Column = document.createElement("div");
+  download_Column.classList.add("col-lg-2");
+  let downloadButton = document.createElement("button");
+  downloadButton.classList.add("btn", "btn-outline-danger", "rounded-pill", "px-4", "mt-5");
+  downloadButton.innerText = "Download";
+  downloadButton.setAttribute("target", "_blank");
+  downloadButton.onclick = function () {
+    goToGamePage(game);
+  }
+ 
+  // Add button to culumn and add column to the row
+  download_Column.appendChild(downloadButton);
+  library_Row.appendChild(download_Column);
+  libraryContainer.appendChild(library_Row);
+
+  // Create a line under the column
+  let lineDiv = document.createElement("div");
+  lineDiv.classList.add("line");
+  let line = document.createElement("hr");
+  line.classList.add("text-light");
+  line.style.width = "100%";
+  // Add line to div and div to row
+  lineDiv.appendChild(line);
+  library_Row.appendChild(lineDiv);
+}
+
+// Select five games from the games array
+const games_To_put_In_Library = games.slice(10, 15);
+
+// Add each game to the library
+games_To_put_In_Library.forEach(game => {
+  libraryGames(game);
+})
+
+function goToGamePage(gameLibrary) {
+    window.location.href = gameLibrary.game_url;
+}
+
+
+
+function goToBrowse() {
     window.location.href = '/browse.html';
-});
+}
+viewYourLibraryButton.addEventListener('click', goToBrowse);
+discoverPopularBtn.addEventListener('click', goToBrowse);
+browseNowBtn.addEventListener('click', goToBrowse);
 
 
 // crete function for the most popular cards at hte home page
@@ -48,7 +157,9 @@ function mostPopularCard(game) {
     "py-3",
     "px-3",
     "rounded-5"
-  );
+    );
+    cardDiv.style.position = "relative";
+    cardDiv.style.display = "inline-block"
   let cardImage = document.createElement("img");
   cardImage.classList.add("card-img", "rounded-3");
   cardImage.src = game.thumbnail;
@@ -67,7 +178,18 @@ function mostPopularCard(game) {
   rateNumb.classList.add("text-light", "ms-2");
   rateNumb.innerText = "4.6";
   cardTitleDiv.appendChild(rateNumb);
-  cardDiv.appendChild(cardTitleDiv);
+    cardDiv.appendChild(cardTitleDiv);
+    let visitButton = document.createElement("button");
+    visitButton.classList.add("btn", "btn-danger", "rounded-pill");
+    visitButton.innerText = "Visit";
+    visitButton.style.left = "30%";
+    visitButton.style.top = "50%";
+    visitButton.style.position = "relative";
+    visitButton.style.display = "none";
+    visitButton.onclick = function () {
+        window.location.href = game.game_url;
+    }
+    cardDiv.appendChild(visitButton);
   // Crete the developer name and the download under the card
   let developerDiv = document.createElement("div");
   developerDiv.classList.add("card-title", "d-flex");
@@ -84,7 +206,13 @@ function mostPopularCard(game) {
   developerDiv.appendChild(downloadNumber);
   cardDiv.appendChild(developerDiv);
   colThree.appendChild(cardDiv);
-  homeMostPopularRow.appendChild(colThree);
+    homeMostPopularRow.appendChild(colThree);
+    cardDiv.addEventListener('mouseover', function () {
+        visitButton.style.display = "block";
+    })
+    cardDiv.addEventListener("mouseleave", function () {
+      visitButton.style.display = "none";
+    });
 }
 
 function choosePopular() {
